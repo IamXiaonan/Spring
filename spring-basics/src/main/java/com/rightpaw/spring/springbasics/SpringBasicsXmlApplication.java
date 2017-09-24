dbe2b2c372fd51dff661a5014fcdab098df2c8e6
MissWan <alinawxn@gmail.com>
2017-09-24 18:42:43 -0500

    mix xml context with application context

--- spring-basics/src/main/java/com/rightpaw/spring/springbasics/SpringBasicsXmlApplication.java
+++ spring-basics/src/main/java/com/rightpaw/spring/springbasics/SpringBasicsXmlApplication.java
@@ -1,21 +1,16 @@
 package com.rightpaw.spring.springbasics;
 
-import com.rightpaw.spring.springbasics.basic.BinarySearchImpl;
-import com.rightpaw.spring.springbasics.xml.XmlPersonDao;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringBasicsXmlApplication {
     public static void main(String[] args)
     {
         try (ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml"))
         {
-            XmlPersonDao personDao = ac.getBean(XmlPersonDao.class);
-            System.out.println(personDao);
+            Logger LOGGER = LoggerFactory.getLogger(SpringBasicsXmlApplication.class);
+            LOGGER.info("Beans -> {}", (Object) ac.getBeanDefinitionNames());
         }
     }
 }
